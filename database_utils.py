45fddfca9dc7da43944cbe4137a79c9605f6a4ab
kgalal <kg1606@gmail.com>
2023-10-03 22:42:34 +0200

    transaction management

--- database_utils.py
+++ database_utils.py
@@ -74,41 +74,68 @@ def get_or_create(model, defaults=None, **kwargs):
 
 def get_or_create_address(street_name, street_number, city, postal_code):
     """
-    Get or create an address record in the database.
+    Retrieve an address from the database or create it if it doesn't exist.
 
-    Args:
+    This function first checks if the given address exists in the database.
+    If it does, it returns the address and a flag indicating that the address was not created.
+    If the address doesn't exist, it creates the necessary postal hierarchy (district, area, route, post office)
+    and the address itself, then returns the new address and a flag indicating that the address was created.
+
+    Parameters:
     - street_name (str): The name of the street.
-    - street_number (str): The number of the street.
+    - street_number (str): The number of the building on the street.
     - city (str): The name of the city.
-    - postal_code (str): The postal code.
+    - postal_code (int or str): The postal code of the address.
 
     Returns:
-    - tuple: A tuple containing the Address instance and a boolean indicating if the address was created (True) or retrieved (False).
+    - Address: The retrieved or newly created address.
+    - bool: A flag indicating whether the address was created (True) or retrieved (False).
+
+    Raises:
+    - DatabaseError: If there's an issue with the database operations.
+    - APIError: If there's an issue fetching data from the external API.
     """
-    address = retrieve_from_db(Address, street_name=street_name, street_number=street_number, city=city)
-    if address:
-        return address, False
-    else:
-        district = get_or_create(District, number=int(str(postal_code)[0]))
-        area = get_or_create(Area, defaults={'district': district}, number=int(str(postal_code)[:2]))
-        route = get_or_create(Route, defaults={'area': area}, number=int(str(postal_code)[:3]))
-        post_office = get_or_create(PostOffice, defaults={'route': route}, number=int(postal_code))
-        matching_address = api.fetch_address_from_geo_admin(street_name, street_number, postal_code, city)
-        x, y, EGID = matching_address['attrs']['x'], matching_address['attrs']['y'], matching_address['attrs']['featureId']
-        roof_info = api.fetch_roof_info_from_geo_admin(x, y)
-        sonnendach_id = roof_info['featureId']
-        new_address = Address(
-            street_name=street_name,
-            street_number=street_number,
-            city=city,
-            post_office=post_office,
-            egid=EGID,
-            sonnendach_id=sonnendach_id
-        )
-        add_to_db(new_address)
-        if not post_office.bfs:
-            building_info = api.fetch_building_info_from_geo_admin(EGID)
-            BFS = building_info['feature']['attributes']['ggdenr']
-            post_office.bfs = BFS
-            update_db()
-        return new_address, True
+    try:
+        # Start a new transaction
+        db.session.begin()
+
+        address = retrieve_from_db(Address, street_name=street_name, street_number=street_number, city=city)
+        if address:
+            return address, False
+        else:
+            district = get_or_create(District, number=int(str(postal_code)[0]))
+            area = get_or_create(Area, defaults={'district': district}, number=int(str(postal_code)[:2]))
+            route = get_or_create(Route, defaults={'area': area}, number=int(str(postal_code)[:3]))
+            post_office = get_or_create(PostOffice, defaults={'route': route}, number=int(postal_code))
+
+            matching_address = api.fetch_address_from_geo_admin(street_name, street_number, postal_code, city)
+            x, y, EGID = matching_address['attrs']['x'], matching_address['attrs']['y'], matching_address['attrs']['featureId']
+
+            roof_info = api.fetch_roof_info_from_geo_admin(x, y)
+            sonnendach_id = roof_info['featureId']
+
+            new_address = Address(
+                street_name=street_name,
+                street_number=street_number,
+                city=city,
+                post_office=post_office,
+                egid=EGID,
+                sonnendach_id=sonnendach_id
+            )
+            add_to_db(new_address)
+
+            if not post_office.bfs:
+                building_info = api.fetch_building_info_from_geo_admin(EGID)
+                BFS = building_info['feature']['attributes']['ggdenr']
+                post_office.bfs = BFS
+                update_db()
+
+            # Commit the transaction
+            db.session.commit()
+
+            return new_address, True
+
+    except Exception as e:
+        # Rollback the transaction in case of any errors
+        db.session.rollback()
+        raise e
